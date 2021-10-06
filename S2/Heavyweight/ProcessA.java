f86e7aeafabcb86af5c59fe60ec468700dddc437
Oscar Pellicé <oscarpellicemas@gmail.com>
2021-10-06 19:20:14 +0200

    Base S2

--- S2/Heavyweight/ProcessA.java
+++ S2/Heavyweight/ProcessA.java
@@ -1,4 +1,32 @@
 package S2.Heavyweight;
 
 public class ProcessA {
+    private String token;
+    private static final int NUM_LIGHTWEIGHTS = 570;
+    private int answersfromLightweigth;
+
+    public void start(){
+        while(true){
+            while(token == null) listenHeavyweight();
+            for (int i=0; i<NUM_LIGHTWEIGHTS; i++)
+                sendActionToLightweight();
+            while(answersfromLightweigth < NUM_LIGHTWEIGHTS)
+                listenLightweight();
+            token = null;
+            sendTokenToHeavyweight();
+        }
+    }
+
+    private void sendTokenToHeavyweight() {
+    }
+
+    private void listenLightweight() {
+    }
+
+    private void sendActionToLightweight() {
+    }
+
+    private void listenHeavyweight() {
+    }
+
 }
