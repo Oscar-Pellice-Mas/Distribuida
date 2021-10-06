85fd33088159bf378aaab763295e7df753c48b12
Oscar Pellicé <oscarpellicemas@gmail.com>
2021-10-06 19:45:03 +0200

    heavyweight

--- S2/Heavyweight/ProcessA.java
+++ S2/Heavyweight/ProcessA.java
@@ -1,32 +1,49 @@
 package S2.Heavyweight;
 
-public class ProcessA {
-    private String token;
-    private static final int NUM_LIGHTWEIGHTS = 570;
-    private int answersfromLightweigth;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-    public void start(){
-        while(true){
-            while(token == null) listenHeavyweight();
-            for (int i=0; i<NUM_LIGHTWEIGHTS; i++)
-                sendActionToLightweight();
-            while(answersfromLightweigth < NUM_LIGHTWEIGHTS)
-                listenLightweight();
-            token = null;
-            sendTokenToHeavyweight();
+public class ProcessA {
+    private static String token;
+    private static final int NUM_LIGHTWEIGHTS = 3;
+    private static int answersfromLightweigth;
+
+    public static void main(String[] args) {
+        try {
+            Socket clientSocket = new Socket("127.0.0.1", 5000);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            while(true){
+                while(token == null) listenHeavyweight(in);
+                for (int i=0; i<NUM_LIGHTWEIGHTS; i++)
+                    sendActionToLightweight(out);
+                while(answersfromLightweigth < NUM_LIGHTWEIGHTS)
+                    listenLightweight(in);
+                token = null;
+                sendTokenToHeavyweight(out);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    private void sendTokenToHeavyweight() {
+    private static void sendTokenToHeavyweight(PrintWriter out) {
+        out.write("TOKEN");
     }
 
-    private void listenLightweight() {
+    private static void listenLightweight(BufferedReader in) throws IOException {
+        String msg = in.readLine();
+        if (msg.equalsIgnoreCase("TOKEN")) token = "TOKEN";
+        else System.out.println(msg);
     }
 
-    private void sendActionToLightweight() {
+    private static void sendActionToLightweight(PrintWriter out) {
     }
 
-    private void listenHeavyweight() {
+    private static void listenHeavyweight(BufferedReader in) {
     }
 
 }
