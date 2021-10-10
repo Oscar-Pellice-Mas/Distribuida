37beddbe9f442fec535b468cf485371f767cff97
Alejandro <alejandro.monux@students.salle.url.edu>
2021-10-10 12:31:20 +0200

    Chore: Made ProcessA and ProcessB servers

--- S2/Heavyweight/ProcessA.java
+++ S2/Heavyweight/ProcessA.java
@@ -6,12 +6,18 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ProcessA {
-    private static final int NUM_LIGHTWEIGHTS = 3;
+    private static final int NUM_LIGHTWEIGHTS = 0;
+    //private static final int NUM_LIGHTWEIGHTS = 3;
     private static final int PORT_HWA = 5000;
     private static final int PORT_HWB = 6000;
     private static final int STARTING_PORT_LWA = 5001;
+
+
+    static ServerSocket serverSocket= null;
+    static Socket serverAccepter= null;
 
     private static int answersfromLightweigth;
     private static String token;
@@ -21,12 +27,23 @@ public class ProcessA {
     private static PrintWriter out = null;
     private static BufferedReader in = null;
 
+
+    private static void CreateServer(){
+        try {
+            serverSocket = new ServerSocket(PORT_HWB);
+            serverAccepter=serverSocket.accept();//establishes connection
+            //DataInputStream serverDataInputStream=new DataInputStream(serverAccepter.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private static void generateSockets(){
         try {
             lightweights = new Socket[NUM_LIGHTWEIGHTS];
             heavyWeight = new Socket("127.0.0.1", PORT_HWB);
-            PrintWriter out = new PrintWriter(heavyWeight.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(heavyWeight.getInputStream()));
+            out = new PrintWriter(heavyWeight.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(heavyWeight.getInputStream()));
             for (int i = 0; i < NUM_LIGHTWEIGHTS; i++) {
                 lightweights[i] = new Socket("127.0.0.1", STARTING_PORT_LWA + i);
             }
@@ -36,12 +53,18 @@ public class ProcessA {
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         try {
+            CreateServer();
+            System.out.println("Server B ready?");
+            scanner.nextInt();
             generateSockets();
+            scanner.nextInt();
             while(true){
                 while(token == null) listenHeavyweight(in);
                 for (int i=0; i<NUM_LIGHTWEIGHTS; i++)
                     sendActionToLightweight(out);
+                answersfromLightweigth=0;
                 while(answersfromLightweigth < NUM_LIGHTWEIGHTS)
                     listenLightweight(in);
                 token = null;
@@ -58,14 +81,22 @@ public class ProcessA {
 
     private static void listenLightweight(BufferedReader in) throws IOException {
         String msg = in.readLine();
-        if (msg.equalsIgnoreCase("TOKEN")) token = "TOKEN";
+        if (msg.equalsIgnoreCase("TOKEN")){
+            token = "TOKEN";
+            answersfromLightweigth++;
+        }
         else System.out.println(msg);
     }
 
     private static void sendActionToLightweight(PrintWriter out) {
     }
 
-    private static void listenHeavyweight(BufferedReader in) {
+    private static void listenHeavyweight(BufferedReader in) throws IOException {
+        String msg = in.readLine();
+        if (msg.equalsIgnoreCase("TOKEN")){
+            token = "TOKEN";
+            System.out.println("Sóc el Process A i puc parlar.");
+        }else System.out.println(msg);
     }
 
 }
--- S2/Heavyweight/ProcessB.java
+++ S2/Heavyweight/ProcessB.java
@@ -3,27 +3,44 @@ package S2.Heavyweight;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ProcessB {
-    private static final int NUM_LIGHTWEIGHTS = 2;
+    private static final int NUM_LIGHTWEIGHTS = 0;
+    //private static final int NUM_LIGHTWEIGHTS = 2;
     private static final int PORT_HWA = 5000;
     private static final int PORT_HWB = 6000;
     private static final int STARTING_PORT_LWB = 6001;
+
+    static ServerSocket serverSocket= null;
+    static Socket serverAccepter= null;
 
     private static int answersfromLightweigth;
     private static String token;
     private static Socket lightweights[] = new Socket[0];
     private static Socket heavyWeight = null;
-    private static ServerSocket listener = null;
+
+    private static PrintWriter out = null;
+    private static BufferedReader in = null;
+
+
+
+    private static void CreateServer(){
+        try {
+            serverSocket = new ServerSocket(PORT_HWB);
+            serverAccepter=serverSocket.accept();//establishes connection
+            //DataInputStream serverDataInputStream=new DataInputStream(serverAccepter.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private static void generateSockets(){
         try {
-            Socket clientSocket = new Socket("127.0.0.1", PORT_HWA);
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            //listener = new ServerSocket(PORT_HWB);
             lightweights = new Socket[NUM_LIGHTWEIGHTS];
-            //heavyWeight = new Socket("127.0.0.1", PORT_HWA);
+            heavyWeight = new Socket("127.0.0.1", PORT_HWA);
+            out = new PrintWriter(heavyWeight.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(heavyWeight.getInputStream()));
             for (int i = 0; i < NUM_LIGHTWEIGHTS; i++) {
                 lightweights[i] = new Socket("127.0.0.1", STARTING_PORT_LWB + i);
             }
@@ -34,22 +51,22 @@ public class ProcessB {
 
     public static void main(String[] args) {
         //Creació dels socket cap el ligthweight
-        generateSockets();
-
-
+        Scanner scanner = new Scanner(System.in);
         try {
+            CreateServer();
+            System.out.println("Server A ready?");
+            scanner.nextInt();
+            generateSockets();
+            scanner.nextInt();
             while (true) {
-                while (token == null) listenHeavyweight(heavyWeight.getInputStream());
-                for (int i = 0; i < NUM_LIGHTWEIGHTS; i++)
-                    sendActionToLightweight(lightweights[i].getOutputStream());
-
-                answersfromLightweigth = 0;
-
-                while (answersfromLightweigth < NUM_LIGHTWEIGHTS)
-                    listenLightweight(lightweights[answersfromLightweigth].getInputStream());
-
+                while(token == null) listenHeavyweight(in);
+                for (int i=0; i<NUM_LIGHTWEIGHTS; i++)
+                    sendActionToLightweight(out);
+                answersfromLightweigth=0;
+                while(answersfromLightweigth < NUM_LIGHTWEIGHTS)
+                    listenLightweight(in);
                 token = null;
-                sendTokenToHeavyweight(heavyWeight.getOutputStream());
+                sendTokenToHeavyweight(out);
             }
         } catch(IOException e){
             e.printStackTrace();
@@ -62,14 +79,23 @@ public class ProcessB {
 
     private static void listenHeavyweight(BufferedReader in) throws IOException {
         String msg = in.readLine();
-        if (msg.equalsIgnoreCase("TOKEN")) token = "TOKEN";
+        if (msg.equalsIgnoreCase("TOKEN")){
+            token = "TOKEN";
+            System.out.println("Sóc el Process B i puc parlar.");
+        }
         else System.out.println(msg);
     }
 
-    private static void listenLightweight(PrintWriter out) {
+    private static void listenLightweight(BufferedReader in) throws IOException {
+        String msg = in.readLine();
+        if (msg.equalsIgnoreCase("TOKEN")){
+            token = "TOKEN";
+            answersfromLightweigth++;
+        }
+        else System.out.println(msg);
     }
 
-    private static void sendActionToLightweight(BufferedReader in) {
+    private static void sendActionToLightweight(PrintWriter out) {
 
     }
 
