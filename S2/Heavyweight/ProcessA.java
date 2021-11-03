2452bee886120af55ad77560d6285877647ef30a
Alejandro Moñux Bernal <alejandro.monux@students.salle.url.edu>
2021-11-03 19:23:01 +0100

    Chore: Adjusted HWA for GenericServer

--- S2/Heavyweight/ProcessA.java
+++ S2/Heavyweight/ProcessA.java
@@ -1,5 +1,7 @@
 package S2.Heavyweight;
 
+import S2.Utils.GenericServer;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,89 +13,84 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
-public class ProcessA {
-    public static ProcessA Instance = null;
+public class ProcessA extends GenericServer {
+    public ProcessA Instance = null;
 
     public ProcessA (){
         Instance = this;
     }
 
     private static final int NUM_LIGHTWEIGHTS = 3;
-    private static final int PORT_HWA = 5000;
-    private static final int PORT_HWB = 6000;
-    private static final int STARTING_PORT_LWA = 5001;
+
 
     static ServerSocket serverSocket= null;
     static Socket serverAccepter= null;
 
-    private static int answersfromLightweigth;
-    private static String token;
+    private int answersfromLightweigth;
+    private String token;
 
-    private static List<Socket> lightweights;
-    private static Socket heavyWeight = null;
+    private List<Socket> lightweights;
+    private Socket heavyWeight = null;
 
-        // Server socket
-    private static PrintWriter outS = null;
-    private static BufferedReader inS = null;
+        // Server socket - inS i outS ve del pare
+
         // HW socket
-    private static PrintWriter outHW = null;
-    private static BufferedReader inHW = null;
+    private PrintWriter outHW = null;
+    private BufferedReader inHW = null;
         // LW socket
-    private static PrintWriter outLW[] = new PrintWriter[NUM_LIGHTWEIGHTS];
-    private static BufferedReader inLW[] = new BufferedReader[NUM_LIGHTWEIGHTS];
+    private PrintWriter outLW[] = new PrintWriter[NUM_LIGHTWEIGHTS];
+    private BufferedReader inLW[] = new BufferedReader[NUM_LIGHTWEIGHTS];
 
-    private static LinkedList<String> cuaLW = new LinkedList<>();
+    private LinkedList<String> cuaLW = new LinkedList<>();
 
-    private static void CreateServer(){
+    private void CreateServer(){
         try {
             // Connect to HW
-            System.out.print("Creating server socket...");
-            serverSocket = new ServerSocket(PORT_HWA);
-            System.out.println("Done!");
-            serverAccepter = serverSocket.accept(); //establishes connection
-            inS = new BufferedReader(new InputStreamReader(serverAccepter.getInputStream()));
-            outS = new PrintWriter(serverAccepter.getOutputStream(), true);
-            System.out.println("Connection recieved");
+            System.out.print(ANSI_GREEN+"Creating server socket...");
+            CreateServer(PORT_HWA);
+            System.out.println(ANSI_YELLOW+"Done!");
+            waitForClient();
+            System.out.println(ANSI_YELLOW+"Connection recieved");
 
             // Connect to all LW
-            System.out.println("Creating lightweight sockets...");
+            System.out.println(ANSI_GREEN+"Creating lightweight sockets...");
             lightweights = new ArrayList<>();
             for(int i =0; i<NUM_LIGHTWEIGHTS;i++){
-                System.out.print("Waiting for lightweight "+ (i+1) + "...");
+                System.out.print(ANSI_GREEN+"Waiting for lightweight "+ (i+1) + "...");
                 lightweights.add(serverSocket.accept());
                 outLW[i]= new PrintWriter(lightweights.get(i).getOutputStream(), true);
                 inLW[i] = new BufferedReader(new InputStreamReader(lightweights.get(i).getInputStream()));
                 outLW[i].println((i+1));
-                System.out.println("Done!");
+                System.out.println(ANSI_YELLOW+"Done!");
             }
-            System.out.println("Done!");
+            System.out.println(ANSI_YELLOW+"Done!");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void generateSockets(){
+    private void generateSockets(){
         try {
             // Connect to HW
-            System.out.print("Creating connexion to heavyweight B...");
+            System.out.print(ANSI_GREEN+"Creating connexion to heavyweight B...");
             heavyWeight = new Socket("127.0.0.1", PORT_HWB);
             inHW = new BufferedReader(new InputStreamReader(heavyWeight.getInputStream()));
             outHW = new PrintWriter(heavyWeight.getOutputStream(), true);
-            System.out.println("Done!");
+            System.out.println(ANSI_YELLOW+"Done!");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
 
-    public static void main(String[] args) {
+    public void mainFunction(String[] args) {
         token = null;
         try {
             generateSockets();
             CreateServer();
-            System.out.println("Config done!");
+            System.out.println(ANSI_BLUE+"Config done!");
             while(true){
-                System.out.println("Listening...");
+                System.out.println(ANSI_GREEN+"Listening...");
                 while(token == null) listenHeavyweight(inS);
                 for (int i=0; i<NUM_LIGHTWEIGHTS; i++)
                     sendActionToLightweight(outLW[i]);
@@ -108,7 +105,7 @@ public class ProcessA {
         }
     }
 
-    private static void listenHeavyweight(BufferedReader in) throws IOException {
+    private void listenHeavyweight(BufferedReader in) throws IOException {
         String msg = in.readLine();
 
         if (msg.equalsIgnoreCase("TOKEN")){
@@ -117,11 +114,11 @@ public class ProcessA {
         else System.out.println("HW ->" + msg);
     }
 
-    private static void sendTokenToHeavyweight(PrintWriter out) {
+    private void sendTokenToHeavyweight(PrintWriter out) {
         out.println("TOKEN");
     }
 
-    private static void listenLightweight(BufferedReader in) throws IOException {
+    private void listenLightweight(BufferedReader in) throws IOException {
         String msg = in.readLine();
 
         if (msg.equalsIgnoreCase("TOKEN")){
@@ -130,8 +127,15 @@ public class ProcessA {
         else System.out.println("HW ->" + msg);
     }
 
-    private static void sendActionToLightweight(PrintWriter out) {
+    private void sendActionToLightweight(PrintWriter out) {
         out.println("TOKEN");
     }
 
+}
+
+class MainHWA{
+    public static void main(String[] args) {
+        ProcessA HWA = new ProcessA();
+        HWA.mainFunction(args);
+    }
 }
---
+++ S2/Utils/GenericServer.java
@@ -0,0 +1,48 @@
+package S2.Utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class GenericServer extends Thread{
+    protected static final int PORT_HWA= 6000;
+    protected static final int PORT_HWB = 6000;
+    protected static final int STARTING_PORT_LWA = 5001;
+    protected static final String LOCALHOST= "127.0.0.1";
+    /** CODIS DE COLORS **/
+
+    public static final String ANSI_YELLOW = "\u001B[33m"; //Codi de colors per actualitzacions del valor
+    public static final String ANSI_GREEN = "\u001B[32m"; //Codi de colors per missatges de estat
+    public static final String ANSI_BLUE = "\u001B[34m";; //Color per num servers
+    public static final String ANSI_CYAN = "\u001B[36m"; //Color per token
+
+    //Camps del servidor
+    protected ServerSocket serverSocket;
+    protected  Socket serverAccepter= null;
+    protected  PrintWriter outS = null;
+    protected  BufferedReader inS = null;
+
+    protected void CreateServer(int port){
+        try {
+            // Connect to HW
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            System.err.println("Aquest port es troba ocupat");
+            System.exit(0);
+        }
+    }
+
+    protected void waitForClient(){
+        try {
+            serverAccepter = serverSocket.accept(); //establishes connection
+            inS = new BufferedReader(new InputStreamReader(serverAccepter.getInputStream()));
+            outS = new PrintWriter(serverAccepter.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
