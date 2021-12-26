51d50c51e3d5250a637b692beaf5b9f6c4c2aed5
Alejandro Moñux Bernal <43568325+alejandromonux@users.noreply.github.com>
2021-12-26 20:25:44 +0100

    Feat: S5 - Replication working Core-L1

--- S5/Core_Layer/CoreLayer.java
+++ S5/Core_Layer/CoreLayer.java
@@ -1,58 +1,78 @@
 package S5.Core_Layer;
 
-import S2.Lightweight.LightweightB;
+import S5.Utils.Data;
 import S5.Utils.GenericServer;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 public class CoreLayer extends GenericServer {
     //Server Information
     private  int                         myId;
+    private  int                         updates;
+    //Data information
+    private Data                         database;
     //Client information
-    private  ArrayList<Socket>           socketList;
-    private  ArrayList<PrintWriter>      outLWList;
-    private  ArrayList<BufferedReader>   inLWLIST;
+    private  ArrayList<Socket>           clientSocketList;
+    private  ArrayList<PrintWriter>      clientOutList;
+    private  ArrayList<BufferedReader>   clientInList;
     private  ArrayList<ClientConnection> clients;
     //Core Layer Server information
     private  ArrayList<Socket>           coreLayers;
     private  ArrayList<PrintWriter>      coreOutS;
     private  ArrayList<BufferedReader>   coreInS;
+    //Core Layer Client information
+    private  ArrayList<Socket>           coreLayersClient;
+    private  ArrayList<PrintWriter>      coreOutSClient;
+    private  ArrayList<BufferedReader>   coreInSClient;
     //Layer 1 Server information
     private  Socket                      L1Socket;
     private  PrintWriter                 L1OutS;
     private  BufferedReader              L1InS;
+    private  ObjectOutputStream          L1OutOs;
+    private  ObjectInputStream           L1InOs;
 
-    //Data information
-    HashMap<Integer,Integer>             database;
 
     public CoreLayer() {
-        socketList  = new ArrayList<Socket>();
-        outLWList   = new ArrayList<PrintWriter>();
-        inLWLIST    = new ArrayList<BufferedReader>();
-        clients     = new ArrayList<ClientConnection>();
-        coreLayers  = new ArrayList<Socket>();
-        coreOutS    = new ArrayList<PrintWriter>();
-        coreInS     = new ArrayList<BufferedReader>();
-        database    = new HashMap<Integer,Integer>();
+        clientSocketList  = new ArrayList<Socket>();
+        clientOutList     = new ArrayList<PrintWriter>();
+        clientInList      = new ArrayList<BufferedReader>();
+        clients           = new ArrayList<ClientConnection>();
+        coreLayers        = new ArrayList<Socket>();
+        coreOutS          = new ArrayList<PrintWriter>();
+        coreInS           = new ArrayList<BufferedReader>();
+        coreLayersClient  = new ArrayList<Socket>();
+        coreOutSClient    = new ArrayList<PrintWriter>();
+        coreInSClient     = new ArrayList<BufferedReader>();
+        database          = new Data();
     }
-
+    void coreLayerConnection(int i) throws IOException {
+        while (true) {
+            String buffer = coreInS.get(i).readLine();
+            if (buffer.charAt(0)=='w'){
+                String[] transaction = buffer.split("-");
+                replaceValue(Integer.parseInt(transaction[1]),Integer.parseInt(transaction[2]));
+                coreOutS.get(i).println("ack");
+                synchronized (this){
+                    updates++;// Incrementem les updates
+                }
+            }else{
+                int value = getValue(Integer.parseInt(buffer.split("-")[1]),Integer.parseInt(buffer.split("-")[2]));
+                coreOutS.get(i).println(value);
+            }
+        }
+    }
     private void connectToServer(){
         System.out.println(ANSI_GREEN+"Waiting for other Core Layer Servers to connect...");
         for (int i = 0; i < NUM_CORE_LAYER; i++) {
             try{
                 if (i != myId){ // To not connect with the owned id
                     Socket aux = new Socket(LOCALHOST,PORT_CORE+i);
-                    socketList.add(aux);
-                    inLWLIST.add( new BufferedReader(new InputStreamReader(aux.getInputStream())));
-                    outLWList.add(  new PrintWriter(aux.getOutputStream(), true));
-                    outLWList.get(outLWList.size() - 1).println("Core-"+myId);
+                    coreLayersClient.add(aux);
+                    coreInSClient.add( new BufferedReader(new InputStreamReader(aux.getInputStream())));
+                    coreOutSClient.add(  new PrintWriter(aux.getOutputStream(), true));
+                    coreOutSClient.get(coreOutSClient.size() - 1).println("Core-"+myId);
                 }
             }catch (IOException e){
                 try {
@@ -70,88 +90,129 @@ public class CoreLayer extends GenericServer {
             //Connectem a B1 o B2
             while (true) {
                 try {
-                    L1Socket = new Socket(LOCALHOST, PORT_L1 + (myId == 2 ? 1 : 2));
+                    L1Socket = new Socket(LOCALHOST, PORT_L1 + (myId == 2 ? 0 : 1));
                     L1InS = new BufferedReader(new InputStreamReader(L1Socket.getInputStream()));
                     L1OutS = new PrintWriter(L1Socket.getOutputStream(), true);
+                    OutputStream os =   L1Socket.getOutputStream();
+                    L1OutOs =           new ObjectOutputStream(os);
+                    InputStream is =    L1Socket.getInputStream();
+                    L1InOs =            new ObjectInputStream(is);
+                    System.out.println(ANSI_YELLOW+"Connectat al Layer 1!");
                     break;
                 }catch (IOException e){
                         System.out.println(ANSI_CYAN+"There has been an error connecting to Layer 1. We will try again");
                 }
             }
         }
-
-
     }
-
-    public void work(int id){
+    private void serverFunction(){
+        String buffer;
+        while(true){
+            try {
+                waitForClient();
+                clientSocketList.add(serverAccepter);
+                clientOutList.add(outS);
+                clientInList.add(inS);
+                buffer = inS.readLine();
+                switch (buffer.split("-")[0].toLowerCase()) {
+                    case "client" -> {
+                        ClientConnection c = new ClientConnection(inS, outS, this);
+                        c.start();
+                        clients.add(c);
+                    }
+                    case "core" -> {
+                        coreLayers.add(serverAccepter);
+                        coreOutS.add(outS);
+                        coreInS.add(inS);
+                        //Layer connection, start
+                        new Thread(
+                                () ->{
+                                    try {
+                                        coreLayerConnection(coreLayers.size()-1);
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                        ).start();
+                        System.out.println(ANSI_YELLOW+"S'ha connectat un server Core");
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void work(int id) throws InterruptedException, IOException {
         myId = id;
         //Obrim el servidor
         CreateServer(PORT_CORE+myId);
         //Obrim un thread per escoltar els clients que entrin
         new Thread(
-                () -> {
-                    String buffer;
-                    while(true){
-                        try {
-                            waitForClient();
-                            socketList.add(serverAccepter);
-                            outLWList.add(outS);
-                            inLWLIST.add(inS);
-                            buffer = inS.readLine();
-                            switch (buffer.split("-")[0].toLowerCase()) {
-                                case "client" -> {
-                                    ClientConnection c = new ClientConnection(inS, outS, this);
-                                    c.start();
-                                    clients.add(c);
-                                }
-                                case "core" -> {
-                                    coreLayers.add(serverAccepter);
-                                    coreOutS.add(outS);
-                                    coreInS.add(inS);
-                                    //TODO: Layer connection
-                                    System.out.println(ANSI_YELLOW+"S'ha connectat un server Core");
-                                }
-
-                                case "layer1" -> {
-                                    //FIXME: Creo que desde la Layer 1 nunca se conectarán hacia arriba
-                                    L1Socket = serverAccepter;
-                                    L1OutS = outS;
-                                    L1InS = inS;
-                                    //TODO: Layer connection
-                                }
-
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
+                this::serverFunction
         ).start();
         //Connectem a servers de la layer
         connectToServer();
         mainFunction();
     }
 
-    private void mainFunction() {
+    private void mainFunction() throws InterruptedException, IOException {
         //TODO: Una funció principal que fagi la fumada de la repliació
+        while (true){
+            synchronized (this){
+                if (updates >= 10 ){
+                    updates   = 0;
+                    if (myId != 1) {
+                        L1OutOs.writeObject(database);
+                        L1OutOs.reset(); //Resetejem per a que envii el objecte modificat
+                        System.out.println(ANSI_YELLOW+"Update sent to L1");
+                    }
+                    System.out.println(ANSI_BLUE+"10 updates!");
+                }
+
+                //System.out.println(ANSI_GREEN+"Num of updates: " + updates);
+            }
+            sleep(4000);
+        }
     }
 
     private int getValue(int layer, int position){
+        //TODO: Que se puedan hacer reads layers 2
+        if (layer == 0){
+            if (database.getDatabase().containsKey(position)){
+                database.getDatabase().get(position);
+            }
+        }else{
+            try {
+                if (myId != 1){
+                L1OutS.println("r-"+position);
+                    int value = Integer.parseInt(L1InS.readLine());
+                    L1OutS.println("ack");
+                    return value;
+                }else{
+                    coreOutSClient.get(0).println("r-"+layer+"-"+position);
+                    int value = Integer.parseInt(coreInSClient.get(0).readLine());
+                    coreOutSClient.get(0).println("ack");
+                    return value;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         return 0;
     }
 
     private void replaceValue(int position, int value){
-        if (database.containsKey(position)){
-            database.replace(position,value);
+        if (database.getDatabase().containsKey(position)){
+            database.getDatabase().replace(position,value);
         }else{
-            database.put(position,value);
+            database.getDatabase().put(position,value);
         }
     }
 
     class ClientConnection extends Thread{
-        private BufferedReader inS;
-        private PrintWriter outS;
-        private CoreLayer instance;
+        private BufferedReader  inS;
+        private PrintWriter     outS;
+        private CoreLayer       instance;
 
         public ClientConnection(BufferedReader inS, PrintWriter outS, CoreLayer inst) {
             this.inS = inS;
@@ -175,12 +236,23 @@ public class CoreLayer extends GenericServer {
                         transaction = buffer.split("-");
 
                         if (transaction[0].equals("r")){
-                            //TODO: Que se puedan hacer reads de otras layers
                             outS.println(instance.getValue(layer, Integer.parseInt(transaction[1])));
                         }else{
                             instance.replaceValue(Integer.parseInt(transaction[1]),Integer.parseInt(transaction[2]));
+                            //Actualizar toda la coreLayer Aquí. EAGER ACTIVE REPLICATION + UPDATE EVERYWHERE.
+                            for (int i = 0; i < coreOutSClient.size(); i++){
+                                    coreOutSClient.get(i).println(buffer);
+                            }
+                            //Esperem a rebre tots els ack
+                            for (int i = 0; i < coreInSClient.size(); i++){
+                                    buffer = coreInSClient.get(i).readLine();
+                                    if (!buffer.equals("ack")) i--;
+                            }
+                            //Fer el ack al client
                             outS.println("ack");
-                            //TODO: Actualizar toda la coreLayer Aquí???
+                            synchronized (this){
+                                updates++;// Incrementem les updates
+                            }
                         }
                     }
 
@@ -198,6 +270,10 @@ class mainCore {
     public static void main(String[] args) {
         CoreLayer core = new CoreLayer();
 
-        core.work(Integer.parseInt(args[0]));
+        try {
+            core.work(Integer.parseInt(args[0]));
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- S5/Layer_1/Layer_1.java
+++ S5/Layer_1/Layer_1.java
@@ -1,4 +1,123 @@
 package S5.Layer_1;
 
-public class Layer_1 {
+import S5.Core_Layer.CoreLayer;
+import S5.Utils.Data;
+import S5.Utils.GenericServer;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Layer_1 extends GenericServer {
+    //Server Information
+    private  int                          myId;
+    //Layer 1 Server information
+    private Socket                        coreSocket;
+    private PrintWriter                   coreOutS;
+    private BufferedReader                coreInS;
+    private ObjectOutputStream            coreOutOs;
+    private ObjectInputStream             coreInOs;
+
+    //Layer 2 Server information
+    private ArrayList<Socket>             L2Sockets;
+    private ArrayList<PrintWriter>        L2OutS;
+    private ArrayList<BufferedReader>     L2InS;
+    private ArrayList<ObjectOutputStream> L2OutOs;
+    private ArrayList<ObjectInputStream>  L2InOs;
+
+
+    //Data information
+    private Data      database;
+
+    public Layer_1() {
+        L2Sockets =  new ArrayList<Socket>();
+        L2OutS    =  new ArrayList<PrintWriter>();
+        L2InS     =  new ArrayList<BufferedReader>();
+        L2OutOs   =  new ArrayList<ObjectOutputStream>();
+        L2InOs    =  new ArrayList<ObjectInputStream>();
+        database  =  new Data();
+    }
+    private void getCoreLayer() {
+        System.out.println(ANSI_GREEN+"Esperem a la connexió de Core Layer");
+        waitForClient();
+        try{
+            coreSocket =        serverAccepter;
+            coreOutS =          outS;
+            coreInS =           inS;
+            OutputStream os =   coreSocket.getOutputStream();
+            coreOutOs =         new ObjectOutputStream(os);
+            InputStream is =    coreSocket.getInputStream();
+            coreInOs =          new ObjectInputStream(is);
+        }catch (IOException e){
+            System.out.println(ANSI_YELLOW+"Error en crear entrades i sortides");
+        }
+        System.out.println(ANSI_YELLOW+"connectat amb la Core Layer!");
+    }
+    private void connectToLayer2() {
+        for (int i = 0; i < NUM_L2; i++){
+            try {
+                L2Sockets.add(new Socket(LOCALHOST, PORT_L2+i));
+                L2InS.add(new BufferedReader(new InputStreamReader(L2Sockets.get(L2Sockets.size()-1).getInputStream())));
+                L2OutS.add(new PrintWriter(L2Sockets.get(L2Sockets.size()-1).getOutputStream(), true));
+                L2InOs.add(new ObjectInputStream(L2Sockets.get(L2Sockets.size()-1).getInputStream()));
+                L2OutOs.add(new ObjectOutputStream(L2Sockets.get(L2Sockets.size()-1).getOutputStream()));
+            }catch (IOException e){
+                System.out.println(ANSI_CYAN+"There has been an error connecting to Layer 2. We will try again");
+                i--;
+            }
+        }
+    }
+
+    public void work(int id){
+        myId=id;
+        CreateServer(PORT_L1+id);
+        getCoreLayer();
+        if (id == 1) connectToLayer2();
+        mainFunction();
+    }
+
+    private void listenCoreLayer() {
+        while (true){
+            try {
+                System.out.println(ANSI_GREEN+"Llegint data");
+                Data d = (Data) coreInOs.readObject();
+                System.out.println(ANSI_GREEN+"Ha arribat data");
+                synchronized (this){
+                    database = d;
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void mainFunction() {
+        new Thread(
+                this::listenCoreLayer
+        ).start();
+        //TODO: Una funció principal que fagi la fumada de la repliació
+        System.out.println(ANSI_GREEN+"Llençat el thread de listenCoreLayer!");
+        while (true) {
+            try {
+                sleep(10000);
+                synchronized (this){
+                    for (int i = 0; i < L2OutOs.size(); i++) {
+                        L2OutOs.get(i).writeObject(database);
+                    }
+                }
+            } catch (InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
+
+class mainLayer1 {
+    public static void main(String[] args) {
+        Layer_1 core = new Layer_1();
+
+        core.work(Integer.parseInt(args[0]));
+    }
 }
