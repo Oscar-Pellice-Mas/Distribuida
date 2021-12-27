9cd7b74b3513a634a80ff408e4b3e6ebe24c769a
Alejandro Moñux Bernal <43568325+alejandromonux@users.noreply.github.com>
2021-12-27 15:55:11 +0100

    Feat: S5 - Layer2 implemented

--- S5/Layer_2/Layer_2.java
+++ S5/Layer_2/Layer_2.java
@@ -1,4 +1,99 @@
 package S5.Layer_2;
 
-public class Layer_2 {
+
+import S5.Layer_1.Layer_1;
+import S5.Utils.Data;
+import S5.Utils.GenericServer;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Layer_2 extends GenericServer {
+    //Server Information
+    private  int                          myId;
+    //Data information
+    private Data                          database;
+    //Layer 1 Server information
+    private Socket L1Socket;
+    private PrintWriter L1OutS;
+    private BufferedReader L1InS;
+    private ObjectOutputStream L1OutOs;
+    private ObjectInputStream L1InOs;
+
+    public Layer_2() {
+        database  =  new Data();
+    }
+    private void getL1Layer(){
+        System.out.println(ANSI_GREEN+"Esperem a que es connecti L1");
+        waitForClient();
+        try{
+            L1Socket =        serverAccepter;
+            L1OutS =          outS;
+            L1InS =           inS;
+            OutputStream os =   L1Socket.getOutputStream();
+            L1OutOs =         new ObjectOutputStream(os);
+            InputStream is =    L1Socket.getInputStream();
+            L1InOs =          new ObjectInputStream(is);
+        }catch (IOException e){
+            System.out.println(ANSI_YELLOW+"Error en crear entrades i sortides");
+        }
+        System.out.println(ANSI_YELLOW+"connectat amb la L1 Layer!");
+    }
+    public void work(int id) {
+        myId=id;
+        CreateServer(PORT_L2+myId);
+        getL1Layer();
+        mainFunction();
+    }
+
+    private void listenL1Layer() {
+        while (true){
+            try {
+                System.out.println(ANSI_GREEN+"Llegint data");
+                Data d = (Data) L1InOs.readObject();
+                System.out.println(ANSI_GREEN+"Ha arribat data");
+                synchronized (this){
+                    database = d;
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void mainFunction() {
+        String buffer;
+        new Thread(this::listenL1Layer).start();
+        while(true){
+            try {
+                buffer = L1InS.readLine();
+                if (buffer.split("-")[0].equals("r")){
+                    //R-L-ID
+                    int value = getValue(Integer.parseInt(buffer.split("-")[2]));
+                    L1OutS.println(value);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private int getValue(int position){
+        synchronized (this){
+            if (database.getDatabase().containsKey(position)){
+                int value = database.getDatabase().get(position);
+                return value;
+            }
+        }
+        return 0;
+    }
+}
+
+
+class mainLayer2 {
+    public static void main(String[] args) {
+        Layer_2 core = new Layer_2();
+
+        core.work(Integer.parseInt(args[0]));
+    }
 }
